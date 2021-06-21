f5418e3089774ae1a865c89aba25331978da4c71
KR0CKY <77172173+KR0CKY@users.noreply.github.com>
2021-06-21 14:46:33 +0300

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,68 @@
+from http.client import HTTPConnection
+import urllib
+
+headers = {
+	'User-agent' : 'thisPython',
+	'Accept-Charset' : 'utf-8'
+}
+print('Enter server address in format [IP:port] ')
+url = input()
+answer = ''
+while (answer != '0'):
+	print('What do you want to do?\n1 - get file, 2 - rewrite file, 3 - add to file, 4 - delete file, 5 - copy file, 6 - move file, 0 - exit')
+	answer = input()
+	try:
+		connection = HTTPConnection(url)
+		response = ''
+		if (answer == '1'):
+			print('Enter filename to get')
+			filename = input()
+			print('Enter the path to save your file')
+			path = input()
+			connection.request('GET', '/' + filename, headers=headers)
+			response = connection.getresponse()
+			content = response.read()
+			print('HTTP/1.0' if response.version == 10 else 'HTTP/1.1', response.status, response.reason)
+			if (response.status == 200):
+				f = open(path + "\\" + filename, "wb")
+				f.write(content)
+				f.close()
+		if (answer == '2'):
+			print('Enter filename to rewrite')
+			filename = input()
+			print('Enter content')
+			body = {'content' : input()}
+			connection.request('PUT', '/' + filename, headers=headers, body=urllib.parse.urlencode(body))
+		if (answer == '3'):
+			print('Enter filename to add')
+			filename = input()
+			print('Enter content')
+			body = {'content' : input()}
+			connection.request('POST', '/' + filename, headers=headers, body=urllib.parse.urlencode(body))
+		if (answer == '4'):
+			print('Enter filename to delete')
+			connection.request('DELETE', '/' + input(), headers=headers)
+		if (answer == '5'):
+			print('Enter filename to copy')
+			filename = input()
+			print('Enter new path')
+			path = input()
+			print('Enter new filename')
+			newFilename = input()
+			body = {'newPath' : path, 'newFilename' : newFilename}
+			connection.request('COPY', '/' + filename, headers=headers, body=urllib.parse.urlencode(body))
+		if (answer == '6'):
+			print('Enter filename to move')
+			filename = input()
+			print('Enter new path')
+			body = {'newPath' : input()}
+			connection.request('MOVE', '/' + filename, headers=headers, body=urllib.parse.urlencode(body))
+		if (answer in {'2', '3', '4', '5', '6'}):
+			response = connection.getresponse()
+			print('HTTP/1.0' if response.version == 10 else 'HTTP/1.1', response.status, response.reason)
+			print(response.getheader('message'))
+			response.read()
+		connection.close()
+	except ConnectionRefusedError:
+		print('Failed to connect')
+		answer = '0'
---
+++ Server.java
@@ -0,0 +1,174 @@
+package com.company;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.*;
+import java.net.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class Server {
+
+    private static String IP_ADDRESS = "localhost";
+    private static String dir = Paths.get("D:\\").toAbsolutePath().toString();
+    private static int status = 200;
+    private static byte[] response;
+
+    static {
+        try {
+            IP_ADDRESS = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        boolean ok = false;
+        while (!ok){
+            try {
+                System.out.println("Enter port number");
+                Scanner in = new Scanner(System.in);
+                HttpServer server = HttpServer.create(new InetSocketAddress(in.nextInt()), 5);
+                ok = true;
+                System.out.println(IP_ADDRESS + ":" + server.getAddress().getPort());
+                server.createContext("/", new MyHandler());
+                server.setExecutor(null); // creates a default executor
+                server.start();
+            }catch (InputMismatchException e){
+                System.out.println("Wrong input");
+            }catch (BindException e){
+                System.out.println("Port is already in use");
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+
+            response = new byte[0];
+            String requestURI = exchange.getRequestURI().toString();
+            System.out.println(exchange.getRequestMethod() + " " + exchange.getRequestURI() + " " + exchange.getProtocol());
+            Headers headers = exchange.getRequestHeaders();
+            for (String key : headers.keySet()){
+                System.out.println(key + " : " + headers.get(key));
+            }
+
+            String filename = requestURI.substring(requestURI.lastIndexOf('/') + 1);
+            try {
+                switch (exchange.getRequestMethod()) {
+
+                    //чтение файла
+                    case "GET":
+                        doGet(filename);
+                        break;
+
+                    //добавление в конец файла
+                    case "POST":
+                        doPost(exchange, filename);
+                        break;
+
+                    //перезапись файла
+                    case "PUT":
+                        doPut(exchange, filename);
+                        break;
+
+                    //удаление файла
+                    case "DELETE":
+                        doDelete(filename);
+                        break;
+
+                    //перемещение файла
+                    case "MOVE":
+                        doMove(exchange, filename);
+                        break;
+
+                    //копирование файла
+                    case "COPY":
+                        doCopy(exchange, filename);
+                        break;
+                    default:
+                        status = 405;
+                }
+            }catch (FileNotFoundException | NoSuchFileException e) {
+                status = 404;
+            } catch (IOException e){
+                status = 400;
+            }
+            exchange.sendResponseHeaders(status, response.length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response);
+            os.close();
+        }
+
+        private static void doGet(String filename) throws  IOException{
+            response = Files.readAllBytes(Paths.get(dir + filename));
+        }
+
+        private static void doPost(HttpExchange exchange, String filename) throws IOException{
+            Path path = Paths.get(dir + filename);
+            if (!Files.exists(path)){
+                Files.createFile(path);
+            }
+            Files.write(path, getParams(exchange.getRequestBody(), new String[]{"content"})[0].getBytes(), StandardOpenOption.APPEND);
+        }
+
+        private static void doPut(HttpExchange exchange, String filename) throws IOException{
+            Path path = Paths.get(dir + filename);
+            if (!Files.exists(path)){
+                Files.createFile(path);
+            }
+            Files.write(path, getParams(exchange.getRequestBody(), new String[]{"content"})[0].getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+        }
+
+        private static void doDelete(String filename) throws IOException{
+            Files.delete(Paths.get(dir + filename));
+        }
+
+        private static void doMove(HttpExchange exchange, String filename) throws IOException{
+            Path path = Paths.get(getParams(exchange.getRequestBody(), new String[]{"newPath"})[0] + filename);
+            if (Files.exists(path)){
+                exchange.getResponseHeaders().put("message", new ArrayList<>(Arrays.asList("File with new name is already exists ")));
+                throw new IOException();
+            }
+            Files.move(Paths.get(dir + filename), path);
+        }
+
+        private static void doCopy(HttpExchange exchange, String filename) throws IOException{
+            String[] params = getParams(exchange.getRequestBody(), new String[]{"newPath", "newFilename"});
+            Path path = Paths.get(params[0] + params[1]);
+            if (Files.exists(path)){
+                exchange.getResponseHeaders().put("message", new ArrayList<>(Collections.singletonList("File with new name is already exists ")));
+                throw new IOException();
+            }
+            Files.copy(Paths.get(dir + filename), path);//, StandardCopyOption.REPLACE_EXISTING);
+        }
+
+        private static String[] getParams(InputStream body, String[] params) throws IOException{
+            StringBuilder bodystr = new StringBuilder();
+            BufferedReader in = new BufferedReader(new InputStreamReader(body));
+            String line;
+            while ((line = in.readLine()) != null) {
+                bodystr.append(URLDecoder.decode(line));
+            }
+
+            String[] result = new String[params.length];
+            int i = 0;
+            for (String param : params) {
+                int start = bodystr.indexOf(param + "=") + param.length() + 1;
+                if (start > bodystr.lastIndexOf("&")) {
+                    result[i++] = bodystr.substring(start);
+                } else {
+                    int end = bodystr.substring(start).indexOf("&");
+                    result[i++] = bodystr.substring(start, end + start);
+                }
+            }
+            in.close();
+            return result;
+        }
+    }
+}
